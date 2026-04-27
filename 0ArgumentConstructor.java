8d347eb671673653453189756a210e5022c11df0
zebacodes <ayesharahaman2598@gmail.com>
2026-04-27 17:57:08 +0530

    0ArgumentConstructor.java

---
+++ 0ArgumentConstructor.java
@@ -0,0 +1,9 @@
+class Test {
+    Test(){
+        System.out.println("Zero argument constructor called");
+    }
+
+    public static void main(String[] args) {
+        Test obj = new Test();
+    }
+}
